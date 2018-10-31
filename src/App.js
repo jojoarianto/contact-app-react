b5a5a0c6cf72eee33c00d6c140d30d72e2fb2f35
Joko Irianto <irianto94@outlook.com>
2018-10-31 22:34:39 +0700

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Contact App</h1>
+        <Header branding="Contact App" />
       </div>
     );
   }
